2a8f300cfc3d2ec0f533ea391696d1b656350cb1
AHMED RIYADH MOHAMMED AHMED <alahmedriyadh1993@gmail.com>
2026-05-05 06:08:02 +0300

    refactor: remove unused rendering import from index.dart

--- lib/index.dart
+++ lib/index.dart
@@ -4,7 +4,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_management/controllers/auth_controller.dart';
